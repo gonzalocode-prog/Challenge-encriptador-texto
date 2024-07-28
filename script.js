1bbc84dfdefd5c7dc8f46604cf673090b834a575
Gonzalo Herrera <gonzaloherrera.tbe@gmail.com>
2024-07-28 01:51:26 -0300

    Challenge encriptador de texto
    Implementacion de codigo para encriptar y desencriptar texto con funcionalidad de copiado de texto.

---
+++ script.js
@@ -0,0 +1,77 @@
+
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+  
+  
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado";
+    parrafo.textContent = textoCifrado;
+    document.getElementById('copiar').removeAttribute('hidden');
+    document.getElementById("muñeco").style.display = 'none';
+    
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+
+  }
+
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado";
+      parrafo.textContent = "";
+      document.getElementById('copiar').removeAttribute('hidden');
+      
+    
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}    
+
+function copiarTexto() {
+  
+  var copyText = document.getElementById("texto");
+
+  
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); 
+
+  
+  navigator.clipboard.writeText(copyText.value);
+  
+  
+  alert("texto copiado: " + copyText.value);
+}
+
+
